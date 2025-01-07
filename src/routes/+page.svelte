5abb149196fde085776806e4dfe8e5f2e56640cc
cyteon <cyteon@hackclub.app>
2025-01-07 15:27:53 +0100

    yes yes coming soon soon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex h-screen w-full">
+    <h1 class="m-auto font-bold text-7xl">Coming Soon</h1>
+</div>
