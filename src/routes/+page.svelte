8bd8ed4d3ee7274259f10c8d6759057f38eea8ca
cyteon <cyteon@hackclub.app>
2025-01-12 12:46:38 +0100

    frontpage etc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,60 @@
-<div class="flex h-screen w-full">
-    <h1 class="m-auto font-bold text-7xl">Coming Soon</h1>
+<script lang="ts">
+    import { ExternalLink, Copyright, CircleDollarSign, Megaphone, Code } from "lucide-svelte";
+    import state from "$lib/state.svelte";
+
+    let getStartedHovered = false;
+
+    const features = [
+        {
+            title: "Open Source",
+            description: "Licensed under the AGPL-3.0",
+            icon: Copyright
+        },
+        {
+            title: "100% Free",
+            description: "We wont ask for a cent",
+            icon: CircleDollarSign
+        },
+        {
+            title: "Privacy",
+            description: "We dont sell your data to meanies (or anyone)",
+            icon: Megaphone
+        },
+        {
+            title: "WIP",
+            description: "New features are being added all the time",
+            icon: Code
+        }
+    ]
+</script>
+
+<div class="flex flex-col h-screen w-full">
+    <div class="text-center w-full mt-2">
+        <h1 class="bg-gradient-to-r text-transparent bg-clip-text from-ctp-yellow to-ctp-green from-20% font-bold text-8xl">Accord</h1>
+        <p class="text-xl mt-2 mb-5">It's time to ditch Discord and Slack</p>
+
+        <a 
+            href={state.user ? "/app" : "/register"}
+            class="unique w-fit mx-auto flex bg-ctp-yellow text-xl text-ctp-crust py-2 px-3 rounded-md"
+            on:mouseenter={() => getStartedHovered = true} on:mouseleave={() => getStartedHovered = false}
+        >
+            {state.user ? "Open Accord" : "Get Started"}
+            <ExternalLink class={`my-auto ${getStartedHovered ? "w-5 h-5 ml-2" : "w-0 h-0"} transition-all duration-300`} />
+        </a>
+        
+        <div class="flex flex-col mt-24">
+            <div class="grid grid-cols-1 md:grid-cols-2 md:mx-auto px-2 md:w-1/2 gap-4">
+                {#each features as f}
+                    <div class="bg-ctp-mantle p-4 rounded-md m-2 flex flex-col border hover:border-ctp-blue transition-all duration-500">
+                        <div class="flex">
+                            <h2 class="text-2xl font-bold">{f.title}</h2>
+                            <f.icon class="w-12 h-12 p-2 border rounded-md bg-ctp-base ml-auto mb-2" />
+                        </div>
+                        
+                        <p class="text-xl text-start">{f.description}</p>
+                    </div>
+                {/each}
+            </div>
+        </div> 
+    </div>
 </div>
--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -9,7 +9,8 @@
 <div class="flex h-screen w-full">
     <SideBar />
 
-    <div class="p-2 bg-ctp-mantle border-r">
+    <div class="p-2 bg-ctp-mantle border-r w-[14.1rem]">
+        <!--
         <button 
             class={`change-view-btn ${currentView === "friends" ? "bg-ctp-surface0/25" : "hover:bg-ctp-surface0/20"}`}
             on:click={() => currentView = "friends"}
@@ -17,31 +18,45 @@
             <PersonStanding class="w-8 h-8" />
             <span class="my-auto">Friends</span>
         </button>
+        -->
     </div>
 
-    {#if currentView === "friends"}
-        <div class="bg-ctp-mantle w-full h-fit border-b p-2 flex">
-            <button 
-                class={`change-view-btn-sm ${currentFriendView === "online" ? "bg-ctp-surface0/25" : "hover:bg-ctp-surface0/20"}`}
-                on:click={() => currentFriendView = "online"}
-            >Online</button>
+    <div class="w-full h-full flex flex-col">
+        <!--
+        {#if currentView === "friends"}
+            <div class="bg-ctp-mantle w-full h-fit border-b p-2 flex">
+                <button 
+                    class={`change-view-btn-sm ${currentFriendView === "online" ? "bg-ctp-surface0/25" : "hover:bg-ctp-surface0/20"}`}
+                    on:click={() => currentFriendView = "online"}
+                >Online</button>
 
-            <button 
-                class={`change-view-btn-sm ml-2 ${currentFriendView === "all" ? "bg-ctp-surface0/25" : "hover:bg-ctp-surface0/20"}`}
-                on:click={() => currentFriendView = "all"}
-            >All</button>
+                <button 
+                    class={`change-view-btn-sm ml-2 ${currentFriendView === "all" ? "bg-ctp-surface0/25" : "hover:bg-ctp-surface0/20"}`}
+                    on:click={() => currentFriendView = "all"}
+                >All</button>
 
-            <button 
-                class={`change-view-btn-sm ml-2 ${currentFriendView === "pending" ? "bg-ctp-surface0/25" : "hover:bg-ctp-surface0/20"}`}
-                on:click={() => currentFriendView = "pending"}
-            >Pending</button>
+                <button 
+                    class={`change-view-btn-sm ml-2 ${currentFriendView === "pending" ? "bg-ctp-surface0/25" : "hover:bg-ctp-surface0/20"}`}
+                    on:click={() => currentFriendView = "pending"}
+                >Pending</button>
 
-            <button 
-                class={`change-view-btn-sm text-ctp-green ml-2 ${currentFriendView === "add" ? "bg-ctp-surface0/25" : "hover:bg-ctp-surface0/20"}`}
-                on:click={() => currentFriendView = "add"}
-            >Add Friend</button>
+                <button 
+                    class={`change-view-btn-sm text-ctp-green ml-2 ${currentFriendView === "add" ? "bg-ctp-surface0/25" : "hover:bg-ctp-surface0/20"}`}
+                    on:click={() => currentFriendView = "add"}
+                >Add Friend</button>
+            </div>
+        {/if}
+        -->
+
+        <div class="text-center mt-4 flex flex-col h-full">
+            <h2 class="text-6xl font-bold">Welcome to Accord!</h2>
+            <p class="text-2xl">Get started by heading to a server and going into a channel</p>
+
+            <div class="mt-auto mb-4">
+                <a href="https://github.com/cyteon/accord" target="_blank" class="text-lg">View the source code</a>
+            </div>
         </div>
-    {/if}
+    </div>
 </div>
 
 <style>
